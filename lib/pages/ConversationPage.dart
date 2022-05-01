679b1da3c4038e36cff3fb84ef875014b8ed9399
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-01 14:52:38 +0300

    continue from here: https://medium.com/@adityadroid/60-days-of-flutter-day-3-4-building-a-chat-screen-in-flutter-e2ed36388dc7

--- lib/pages/ConversationPage.dart
+++ lib/pages/ConversationPage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:messio/widgets/ChatAppBar.dart';
-import 'package:messio/widgets/ChatListWidget.dart';
-import 'package:messio/widgets/InputWidget.dart';
 
 class ConversationPage extends StatefulWidget {
   @override
